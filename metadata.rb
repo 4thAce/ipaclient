c3b19832a19cd48354d2ada151c720d1022fc688
Rich Magahiz <frabdango@magahiz.com>
2014-01-31 20:28:49 +0000

    Converting ruby script to chef

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ipaclient'
+maintainer       'Infochimps, a CSC Big Data Business'
+maintainer_email 'richard.magahiz@infochimps.com'
+license          'Apache 2.0'
+description      'Installs/Configures ipaclient'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
