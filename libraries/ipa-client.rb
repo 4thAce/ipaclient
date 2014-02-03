e470aa57ffa810f16f2ac7d81e52240aee2f8ec2
Rich Magahiz <frabdango@magahiz.com>
2014-02-03 20:34:43 +0000

    Add templates and entries in default recipe

---
+++ libraries/ipa-client.rb
@@ -0,0 +1,5 @@
+class Chef::Recipe::IPAclient
+  def mknsspwd
+    # Use certutil to create the password file for nss
+  end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+template "/etc/nsswitch.conf" do
+  source "nsswitch.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+template "/etc/sudo-ldap.conf" do
+  source "sudo-ldap.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables (
+    ldapbase: "dc=chimpy,dc=internal",
+    bindpw: "vowelbear"
+  )
+end
+  
+template "/etc/hosts" do
+  source "hosts.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables (
+    masterhostname: "ipamasterdev4"
+    masterip: "50.17.200.2"
+    domain: "chimpy.internal"
+  )
+end
+
+template "/usr/sbin/ipa-client-install" do
+  source "ipa-client-install.erb"
+  mode 0644
+  owner  "root"
+  group "root"
+end
+
+package "libnss3-tools"
+package "freeipa-client"
+package "sssd"
+package "openssh-server"
+
+execute "certutil" do
+  # Set up the password file here
+  command "certutil -N -d /etc/pki/nssdb/ -f #{nsspasswordfile}"
+end
+
+execute "client-install" do
+  pwd_secret = Chef::EncryptedDataBagItem.load_secret("#{SECRETPATH}")
+  ipa_password = Chef::EncryptedDataBagItem.load("passwords", "ipapassword", pwd_secret)
+  command "ipa-client-install --server=#{node[:fqdn]}.#{domain} --domain=#{domain} --realm=#{REALM} --noac --enable-dns-updates --no-ntp --hostname=#{node[:fqdn]} --mkhomedir --password=#{ipa_password} --principal=admin"
+end
