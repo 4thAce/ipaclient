ac58d88302d162bd403209ef2c44b7eb894d1963
Rich Magahiz <frabdango@magahiz.com>
2014-02-06 18:39:14 +0000

    More bugfixes based on tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,10 +17,6 @@
 # limitations under the License.
 #
 
-Chef::Log.info("node fqdn: #{node[:fqdn]}")
-Chef::Log.info("domain: #{node['ipaclient']['domain']}")
-Chef::Log.info("Temp path: #{Chef::Config[:file_cache_path]}")
-
 node.default['openssh']['server']['use_p_a_m'] = 'yes'
 node.default['openssh']['client']['gssapi_delegate_credentials'] = 'yes'
 node.default['openssh']['client']['gssapi_authentication'] = 'yes'
@@ -41,7 +37,7 @@ template "/etc/sudo-ldap.conf" do
   mode 0644
   SECRETPATH = node['ipaclient']['secretpath']
   pwd_secret = Chef::EncryptedDataBagItem.load_secret("#{SECRETPATH}")
-  bindpwd = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", "nss_password")
+  bindpwd = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", pwd_secret)['nss_password']
   variables ({
     :bindpwd => "#{bindpwd}"
   })
@@ -52,7 +48,7 @@ template "/etc/hosts" do
   owner "root"
   group "root"
   mode 0644
-  hostname = node[:fqdn].split('.')[0]
+  hostname = "#{node[:fqdn]}".split('.')[0]
   variables ({
     :hostname => "#{hostname}",
   })
@@ -60,14 +56,14 @@ end
 
 template "/usr/sbin/ipa-client-install" do
   source "ipa-client-install.erb"
-  mode 0644
+  mode 0544
   owner  "root"
   group "root"
 end
 
 template "#{node['ipaclient']['nsspasswordfile']}" do
   pwd_secret = Chef::EncryptedDataBagItem.load_secret("#{SECRETPATH}")
-  nss_password = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", "nss_password")
+  nss_password = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", pwd_secret)['nss_password']
   source "password.erb"
   owner "root"
   group "root"
@@ -77,15 +73,57 @@ template "#{node['ipaclient']['nsspasswordfile']}" do
 })
 end
 
-execute "certutil" do
-  command "certutil -N -d /etc/pki/nssdb/ -f #{node['ipaclient']['nsspasswordfile']}"
+remote_directory "/etc/pki/nssdb" do
+  owner "root"
+  group "root"
+  mode 0744
+  action :create_if_missing
 end
+
+execute "certutil" do
+  command "certutil -N -d /etc/pki/nssdb/ -f #{node['ipaclient']['nsspasswordfile']} < /dev/null"
+  not_if "test -f /etc/pki/nssdb/cert8.db"
+end
+
+#package "python-nss"
+#package "python-sss"
+apt_repository "sssd" do
+  uri "http://ppa.launchpad.net/freeipa/ppa/ubuntu"
+  distribution "precise"
+  components ["main"]
+  key "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4F48C3EDC98C220F"
+  action :add
+end
+
+apt_repository "freeipa" do
+  uri "http://ppa.launchpad.net/freeipa/ppa/ubuntu"
+  distribution "precise"
+  components ["main"]
+  key "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4F48C3EDC98C220F"
+  action :add
+end
+
+package "sssd" do
+  options "--force-yes"
+  action :install
+end
+
+package "freeipa" do
+  action :install
+end
+
+package "openssh" do
+  action :install
+end
+
+#execute "client-uninstall" do
+#end
 
 execute "client-install" do
   # Set up the encrypted data bag
   SECRETPATH = node['ipaclient']['secretpath']
   pwd_secret = Chef::EncryptedDataBagItem.load_secret("#{SECRETPATH}")
-  ipa_password = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", "admin_secret")
-  hostname = node[:fqdn].split('.')[0]
+  ipa_password = Chef::EncryptedDataBagItem.load("passwords", "ipapasswords", pwd_secret)['admin_secret']
+  hostname = "#{node[:fqdn]}".split('.')[0]
   command "ipa-client-install --server=#{hostname}.#{node['ipaclient']['domain']} --domain=#{node['ipaclient']['domain']} --realm=#{node['ipaclient']['realm']} --noac --enable-dns-updates --no-ntp --hostname=#{hostname}.#{node['ipaclient']['domain']} --mkhomedir --password=#{ipa_password} --principal=admin"
 end
