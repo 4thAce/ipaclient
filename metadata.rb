552ec2c979ee9e04d8dc14d120f2a1ac0734b5c3
Rich Magahiz <frabdango@magahiz.com>
2014-02-12 20:00:18 +0000

    Change version depends

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'Apache 2.0'
 description      'Installs/Configures ipaclient'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.3'
-depends          'apt', '= 1.9.0'
-depends          'openssh', '=1.3.2'
+depends          'apt', '~> 1.9.0'
+depends          'openssh', '~> 1.3.2'
 
 recipe           "ipaclient::default",              "Base config"
 recipe           "ipaclient::install_client",       "Install, set up ipa client"
